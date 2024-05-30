f8e02f433ebbcc2df0f3015297b010d94de97815
Luke Maciak <lmaciak@durkingroup.com>
2024-05-29 22:29:41 -0400

    Added limit and head functionality

--- sieve.go
+++ sieve.go
@@ -23,12 +23,17 @@ func main() {
     var column int
     flag.IntVar(&column, "column", 0, "the column to be used for filtering, starting at 0")
 
+    var limit int
+    flag.IntVar(&limit, "limit", 0, "limit the number of filtered lines that are output (if zero, no limit)")
     
     var values string
     flag.StringVar(&values, "values", "", "comma separated list of values to be filtered on")
 
     var ver bool
     flag.BoolVar(&ver, "version", false, "display version number and exit")
+
+    var head bool
+    flag.BoolVar(&head, "head", false, "display first 10 lines of the file, ignore all other options")
 
     var partial bool
     flag.BoolVar(&partial, "partial", false, "use partial search instead of exact match")
@@ -54,15 +59,25 @@ func main() {
 
     scanner := bufio.NewScanner(file)
 
+    counter := 1
+    if head { limit = 10 } // when head is set only display 10 first lines
+
     for scanner.Scan() {
+
+        if limit != 0 && counter >= limit { break }
 
         line := scanner.Text()
 
         cols := strings.Split(line, delimiter)
         
-        if contains(value_list, cols[column], partial) {
-
-            fmt.Println(line)
+        if head { // if head is set just print out every line
+                fmt.Println(line)
+                counter++
+        } else {
+            if contains(value_list, cols[column], partial) {
+                fmt.Println(line)
+                counter++
+            }
         }
     }
 
@@ -77,13 +92,10 @@ func contains(list []string, value string, partial bool) bool {
 
         // partial search
         if partial {
-            if strings.Contains(value, s) { 
-                return true 
-            }
+            if strings.Contains(value, s) { return true }
         } else { // exact match
             if value == s { return true } 
         }
     }
-
     return false
 }
